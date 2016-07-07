5f46a213f783d61a75617adeef394209259c433a
bharathb8 <bharahb@gmail.com>
2016-07-07 14:41:21 -0700

    Commit #1: Initial base class and directory setup

---
+++ com/src/PingStatistics.java
@@ -0,0 +1,23 @@
+import java.lang.System.*;
+
+public class PingStatistics {
+
+	public class FetchURL implements Runnable {
+
+		private static String URL;
+
+		public FetchURL(String url) {
+			this.URL = url;
+
+		}
+
+		public void run() {
+			
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
